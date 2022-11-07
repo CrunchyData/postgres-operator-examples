a58fbd2feb63dcabeeb6b2f98a7f751f793e34de
Nathan Okolita <Nathan.Okolita@indico.io>
2022-11-07 11:34:57 -0500

    Adjust for change in values
    Now there is a new section controllerImages. These images need to be pulled, tagged, and pushed to harbor.

--- push_and_tag.sh
+++ push_and_tag.sh
@@ -1,5 +1,6 @@
 #Grab the images that need to be tagged and then pushed
-IMAGE=$(cat helm/install/values.yaml | shyaml get-value image.image)
+CONTROLLER_IMAGE_CLUSTER=$(cat helm/install/values.yaml | shyaml get-value controllerImages.cluster)
+CONTROLLER_IMAGE_UPGRADE=$(cat helm/install/values.yaml | shyaml get-value controllerImages.upgrade)
 RELATED_IMAGES=$(cat helm/install/values.yaml | shyaml values relatedImages image | awk '{print $2}' | sed '/^$/d')
 
 # docker login
@@ -8,16 +9,29 @@ docker login -u _json_key --password-stdin https://gcr.io < keyfile.json
 echo $HARBOR_PASSWORD | docker login -u $HARBOR_USERNAME --password-stdin https://harbor.devops.indico.io
 # pull images
 
-docker pull $IMAGE
-IMAGE_BASE=$(basename $IMAGE)
+#pull postgres-operator controller image
+docker pull $CONTROLLER_IMAGE_CLUSTER
+IMAGE_BASE=$(basename $CONTROLLER_IMAGE_CLUSTER)
 #push to gcr
-docker tag $IMAGE gcr.io/new-indico/$IMAGE_BASE
+docker tag $CONTROLLER_IMAGE_CLUSTER gcr.io/new-indico/$IMAGE_BASE
 docker push gcr.io/new-indico/$IMAGE_BASE
 #push to harbor
-docker tag $IMAGE harbor.devops.indico.io/indico/$IMAGE_BASE
+docker tag $CONTROLLER_IMAGE_CLUSTER harbor.devops.indico.io/indico/$IMAGE_BASE
 docker push harbor.devops.indico.io/indico/$IMAGE_BASE
 #Remove Junk
-docker rmi $IMAGE gcr.io/new-indico/$IMAGE_BASE harbor.devops.indico.io/indico/$IMAGE_BASE
+docker rmi $CONTROLLER_IMAGE_CLUSTER gcr.io/new-indico/$IMAGE_BASE harbor.devops.indico.io/indico/$IMAGE_BASE
+
+#pull postgres-operator upgrade image
+docker pull $CONTROLLER_IMAGE_UPGRADE
+IMAGE_BASE=$(basename $CONTROLLER_IMAGE_UPGRADE)
+#push to gcr
+docker tag $CONTROLLER_IMAGE_UPGRADE gcr.io/new-indico/$IMAGE_BASE
+docker push gcr.io/new-indico/$IMAGE_BASE
+#push to harbor
+docker tag $CONTROLLER_IMAGE_UPGRADE harbor.devops.indico.io/indico/$IMAGE_BASE
+docker push harbor.devops.indico.io/indico/$IMAGE_BASE
+#Remove Junk
+docker rmi $CONTROLLER_IMAGE_UPGRADE gcr.io/new-indico/$IMAGE_BASE harbor.devops.indico.io/indico/$IMAGE_BASE
 
 for ri in $RELATED_IMAGES;
 do
